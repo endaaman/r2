f9e78ecb4e748d3960e3ff348780b49e7e31024c
endaaman <ken@endaaman.com>
2024-10-21 00:28:24 +0900

    SvelteKit + Prisma

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+import '../app.css';
+</script>
+
+<h1>Title</h1>
+<slot />
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const { iconsPlugin, getIconCollections } = require('@egoist/tailwindcss-icons')
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+    iconsPlugin({
+      collections: getIconCollections(['lucide']),
+    }),
+  ],
+  daisyui: {
+    themes: [
+      // 'light',
+      // 'dark',
+      // 'cupcake',
+      // 'synthwave',
+      // 'corporate',
+      'emerald',
+      // 'winter',
+    ]
+  },
+}
