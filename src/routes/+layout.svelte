a7df1865d13ce5617ddc64c3709ac9b92acdcbf0
endaaman <ken@endaaman.com>
2024-12-31 09:10:56 +0900

    sv@5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,23 @@
-<script>
-import '../app.css';
+<script lang="js">
+import '../app.css'
+
+const { header, children } = $props()
 </script>
 
 <h1>Title</h1>
-<slot />
+
+<ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="/about">About</a></li>
+</ul>
+
+<p>{ !!header }</p>
+{@render header?.()}
+
+---<br>
+
+<!-- <slot></slot> -->
+{@render children()}
+
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Home</h1>
+
+{#snippet header()}
+  <p class="p-20">header</p>
+{/snippet}
 
 <p class="p-20">aa</p>
